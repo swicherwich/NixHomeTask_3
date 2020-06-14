0bcebbcafd2a1da555ce8cadcae0e5809e7f8496
Swicherwich <gitgilfoyle@gmail.com>
2020-06-15 01:44:34 +0300

    added tests

---
+++ userappbuilder/src/test/java/com/nix/userapp/DataBuilderTest.java
@@ -0,0 +1,20 @@
+package com.nix.userapp;
+
+import com.nix.userapp.builder.DataBuilder;
+import com.nix.userapp.service.UserService;
+import com.nix.userapp.service.impl.UserServiceImpl;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DataBuilderTest {
+
+	@Test
+	public void builderTest() {
+		DataBuilder dataBuilder = new DataBuilder();
+		UserService userService = new UserServiceImpl();
+		dataBuilder.buildDataList();
+		
+		assertEquals(3, userService.findAll().size());
+	}
+}
