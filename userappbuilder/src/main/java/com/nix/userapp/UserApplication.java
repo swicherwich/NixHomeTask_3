dc7eb37e61f7f4255644e807dc85b524257218df
Swicherwich <gitgilfoyle@gmail.com>
2020-06-15 10:44:39 +0300

    added some buggy line of code

--- userappbuilder/src/main/java/com/nix/userapp/UserApplication.java
+++ userappbuilder/src/main/java/com/nix/userapp/UserApplication.java
@@ -1,8 +1,6 @@
 package com.nix.userapp;
 
 import com.nix.userapp.builder.DataBuilder;
-import com.nix.userapp.service.UserService;
-import com.nix.userapp.service.impl.UserServiceImpl;
 
 public class UserApplication {
 	public static void main(String[] args) {
